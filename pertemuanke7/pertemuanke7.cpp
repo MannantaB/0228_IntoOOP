76e5f49a1e22e2f2f37abdede4860e95638be0c4
MannantaB <mannanta18@gmail.com>
2024-04-23 15:25:52 +0700

    create prosedur output

--- pertemuanke7/pertemuanke7.cpp
+++ pertemuanke7/pertemuanke7.cpp
@@ -28,6 +28,12 @@ public:
         cout << "SKS: ";
         cin >> sks;
     }
+
+    void output() {
+        cout << "Kode MK: " << kodeMK << endl;
+        cout << "Nama MK: " << namaMK << endl;
+        cout << "SKS: " << sks << endl;
+    }
 };
 
 
