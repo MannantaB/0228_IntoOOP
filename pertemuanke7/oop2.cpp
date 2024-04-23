e38b7a94e6c51b72e53de23c06364a0615cac4bb
MannantaB <mannanta18@gmail.com>
2024-04-23 15:50:06 +0700

    create prosedur input

--- pertemuanke7/oop2.cpp
+++ pertemuanke7/oop2.cpp
@@ -6,6 +6,14 @@ private:
     float panjang, lebar;
 public:
     float luas;
+
+    void input() { // methode input persegi panjang
+
+        cout << "Masukan Panjangnya = ";
+        cin >> panjang;
+        cout << "Masukan Lebarnya = ";
+        cin >> lebar;
+    }
 };
 
 
